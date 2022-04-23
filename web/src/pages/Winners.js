4486e69f52469323004493bee73419a28859b640
mertovich <88425310+mertovich@users.noreply.github.com>
2022-04-23 15:08:42 +0300

    add(Web): Winners

---
+++ web/src/pages/Winners.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import WinnersList from '../components/WinnersList'
+
+export default class Winners extends Component {
+    state = {
+        Winners: [],
+    }
+
+    getWinners = () => {
+        fetch('http://localhost:3000/bot/winners')
+            .then(response => response.json())
+            .then(data => this.setState({ Winners: data }))
+    }
+
+    componentDidMount() { 
+        this.getWinners()
+    }
+
+    render() {
+        return (
+            <div>
+                <WinnersList Winners={this.state.Winners} />
+            </div>
+        )
+    }
+}
