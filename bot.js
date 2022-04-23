61f99866b017f83864b861babe657959fb312dc5
mertovich <88425310+mertovich@users.noreply.github.com>
2022-04-23 14:18:14 +0300

    add(Bot): `raffle` winner

--- bot.js
+++ bot.js
@@ -29,11 +29,26 @@ client.on('message', async message => {
     }
 
     if(message.content == 'raffle'){
+        let date = new Date();
+        let hour = date.getHours();
+        let minute = date.getMinutes();
+        let day = date.getDate();
+        let month = date.getMonth() + 1;
+        let year = date.getFullYear();
+        let time = day+'/'+month+'/'+year+' '+hour+':'+minute;
         let serverID = message.guild.id;
+        let serverName = message.guild.name;
         const list = client.guilds.cache.get(serverID); 
-        const userList = list.members.cache.map(member => member.user.id);
+        const userList = list.members.cache.map(member => {
+            return {
+                id: member.user.id,
+                name: member.user.username,
+                image:`https://cdn.discordapp.com/avatars/${member.user.id}/${member.user.avatar}.png?size=256`
+            }
+        });
         var item = userList[Math.floor(Math.random()*userList.length)];
-        await message.reply(`🎉Winner is <${'\@'+item}>`);
+        dataManager.createWinner(item,time,serverName);
+        await message.reply(`🎉Winner is <${'\@'+item.id}>`);
     }
 
     if (message.content == 'users') {
